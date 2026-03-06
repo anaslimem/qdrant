c33388e493136c40a089278d34439bb167c6b149
jojii <jojii@gmx.net>
2026-03-06 12:03:45 +0100

    Add ignore_deferred flag for internal scroll API

--- lib/segment/src/segment/order_by.rs
+++ lib/segment/src/segment/order_by.rs
@@ -22,6 +22,7 @@ impl Segment {
         condition: &Filter,
         is_stopped: &AtomicBool,
         hw_counter: &HardwareCounterCell,
+        ignore_deferred: bool,
     ) -> OperationResult<Vec<(OrderValue, PointIdType)>> {
         let payload_index = self.payload_index.borrow();
         let id_tracker = self.id_tracker.borrow();
@@ -38,6 +39,8 @@ impl Segment {
 
         let start_from = order_by.start_from();
 
+        let effective_deferred_id = self.deferred_internal_id.filter(|_| !ignore_deferred);
+
         let values_ids_iterator = payload_index
             .iter_filtered_points(
                 condition,
@@ -45,7 +48,7 @@ impl Segment {
                 &cardinality_estimation,
                 hw_counter,
                 is_stopped,
-                self.deferred_internal_id,
+                effective_deferred_id,
             )
             .flat_map(|internal_id| {
                 // Repeat a point for as many values as it has
@@ -93,6 +96,7 @@ impl Segment {
         filter: Option<&Filter>,
         is_stopped: &AtomicBool,
         hw_counter: &HardwareCounterCell,
+        ignore_deferred: bool,
     ) -> OperationResult<Vec<(OrderValue, PointIdType)>> {
         let payload_index = self.payload_index.borrow();
 
@@ -107,7 +111,9 @@ impl Segment {
         let range_iter = numeric_index
             .stream_range(&order_by.as_range())
             // We can't early stop the iterator for deferred points because the items are sorted lexicographically by type `(T, internalID)`.
-            .filter(|&(_, internal_id)| !self.is_point_deferred_internal(internal_id));
+            .filter(|&(_, internal_id)| {
+                ignore_deferred || !self.is_point_deferred_internal(internal_id)
+            });
 
         let directed_range_iter = match order_by.direction() {
             Direction::Asc => Either::Left(range_iter),
